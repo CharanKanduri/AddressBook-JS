8332f1fa32e6a13e599e6ca7274bb7c1c2a9bb60
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-19 22:41:41 +0530

    [CHARAN]'Validation'

--- AddressBook-JS.js
+++ AddressBook-JS.js
@@ -1,3 +1,4 @@
+var prompt=require("prompt-sync")();
 console.log("Welcome to AddressBook JS program\n")
 //UC-1 Creating a Address Book with 
 class ContactClass
@@ -11,30 +12,116 @@ class ContactClass
  phoneNumber;
  eMail;
 
+ 
  //defining Constructor
  constructor(firstName,lastName,address,city,state,zip,phoneNumber,eMail)
- {
-     this.firstName=firstName;
-     this.lastName=lastName;
-     this.address=address;
-     this.city=city;
-     this.state=state;
-     this.zip=zip;
-     this.phoneNumber=phoneNumber;
-     this.eMail=eMail;
+ { 
+     
+    //defining the regular expressions for each Address Attributes 
+    let firstNameRegex = new RegExp('^[A-Z][a-z]{2,}$');
+    let lastNameRegex = new RegExp('^[A-Z][a-z]{2,}$');
+    let addressRegex = new RegExp('^[A-za-z]{4,}$');
+    let eMailRegex = new RegExp('^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$');
+    let zipRegex =RegExp('^[0-9]{5,}$');
+    let phoneNumberRegex= RegExp('^[0-9]{9,}$')
+
+    //performing validation using regex test method
+    if(firstNameRegex.test(firstName))
+    {   
+        this.firstName=firstName;
+        
+    }
+    else
+    {
+        throw 'Enter correct first name';
+    }
+
+    if(addressRegex.test(address))
+    {   
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        
+    }
+    else
+    {
+        throw 'Enter correct Address';
+    }
+
+    if(lastNameRegex.test(lastName))
+    {   
+        this.lastName=lastName;
+        
+    }
+    else
+    {
+        throw 'Enter correct Last name';
+    }
+
+    
+    if(eMailRegex.test(eMail))
+    {   
+        this.eMail=eMail;
+        
+    }
+    else
+    {
+        throw 'Enter correct eMail';
+    }
+    
+    if(zipRegex.test(zip))
+    {   
+        this.zip=zip;
+        
+    }
+    else
+    {
+        throw 'Enter correct zip';
+    }
+
+    if(phoneNumberRegex.test(phoneNumber))
+    {   
+        this.phoneNumber=phoneNumber;
+        
+    }
+    else
+    {
+        throw 'Enter correct phone number';
+    }
+
  }
+
  toString()
  {
-  return ("Name: " + this.firstName +" "+ this.lastName + " \t Address: " +this.address+" \t City: " +this.city+" \t State: "+this.state+" \t Pincode: " +this.zip+" \t Phone Number: "+this.phoneNumber+" \t Email Id: "+this.eMail);
-      
+  return ("Name: " + this.firstName +" "+ this.lastName + " \t Address: " +this.address+" \t City: " +this.city+" \t State: "+this.state+" \t Pincode: " +this.zip+" \t Phone Number: "+this.phoneNumber+" \t Email Id: "+this.eMail);    
  }
+
 }
 //creating an array to store the address book info
-var contactList= new Array();
-let ContactClassObject = new ContactClass("Charan","Kanduri","Saraswathi Nagar","Nellore","AP","524002","939393939","Charan@gmail.com");
-contactList.push(ContactClassObject);
-let ContactClassObject1 = new ContactClass("Srikar","Kanduri","4th Avenue","Boston","Massachusetts","524002","009000900","Srikar@gmail.com");
-contactList.push(ContactClassObject1);
+try
+{
+    var contactList= new Array();
+    var ContactClassObject;
+    var totalContacts = prompt("\nEnter total number of contacts to be added :\n")
+    while(totalContacts--)
+    {
+    
+    let firstName= prompt("Enter First Name:  ");
+    let lastName=prompt("Enter Last Name:  ");
+    let address=prompt("Enter Address:  ");
+    let city=prompt("Enter City:  ");
+    let state=prompt("Enter State:  ");
+    let zip=prompt("Enter Zip:  ");
+    let phoneNumber=prompt("Enter Phone Number:  ");
+    let eMail=prompt("Enter Email:  ");
+    ContactClassObject = new ContactClass(firstName,lastName,address,city,state,zip,phoneNumber,eMail);
+    contactList.push(ContactClassObject);
+    }
+}
+catch(e)
+{
+    console.error(e);
+}
 
 //printing the address book
 contactList.forEach(contact =>{
